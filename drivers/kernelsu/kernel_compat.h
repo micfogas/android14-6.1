96cda8ca5ef658f569f00b1701607764606f811a
technotic <pixie@technotic.us>
2025-11-07 21:52:35 -0800

    add: sukisu main

---
+++ drivers/kernelsu/kernel_compat.h
@@ -0,0 +1,56 @@
+#ifndef __KSU_H_KERNEL_COMPAT
+#define __KSU_H_KERNEL_COMPAT
+
+#include <linux/fs.h>
+#include <linux/version.h>
+#include "ss/policydb.h"
+#include "linux/key.h"
+
+/*
+ * Adapt to Huawei HISI kernel without affecting other kernels ,
+ * Huawei Hisi Kernel EBITMAP Enable or Disable Flag ,
+ * From ss/ebitmap.h
+ */
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)) &&                           \
+        (LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0)) ||               \
+    (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0)) &&                      \
+        (LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0))
+#ifdef HISI_SELINUX_EBITMAP_RO
+#define CONFIG_IS_HW_HISI
+#endif
+#endif
+
+extern long ksu_strncpy_from_user_nofault(char *dst,
+                      const void __user *unsafe_addr,
+                      long count);
+
+extern void ksu_android_ns_fs_check();
+extern struct file *ksu_filp_open_compat(const char *filename, int flags,
+                     umode_t mode);
+extern ssize_t ksu_kernel_read_compat(struct file *p, void *buf, size_t count,
+                      loff_t *pos);
+extern ssize_t ksu_kernel_write_compat(struct file *p, const void *buf,
+                       size_t count, loff_t *pos);
+/*
+ * ksu_copy_from_user_retry
+ * try nofault copy first, if it fails, try with plain
+ * paramters are the same as copy_from_user
+ * 0 = success
+ */
+static long ksu_copy_from_user_retry(void *to, 
+        const void __user *from, unsigned long count)
+{
+    long ret = copy_from_user_nofault(to, from, count);
+    if (likely(!ret))
+        return ret;
+
+    // we faulted! fallback to slow path
+    return copy_from_user(to, from, count);
+}
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 2) // Android backport this feature in 5.10.2
+extern void ksu_seccomp_clear_cache(struct seccomp_filter *filter, int nr);
+extern void ksu_seccomp_allow_cache(struct seccomp_filter *filter, int nr);
+#endif
+
+#endif
